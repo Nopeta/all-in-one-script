c6e39a14a29bf117e390ce3c2a7cb0bfe2bc887b
Shin <noiecat128@gmail.com>
2022-11-03 16:47:05 +0800

    fix: fix node and nvm

--- install-shin-ubuntu.sh
+++ install-shin-ubuntu.sh
@@ -70,10 +70,22 @@ install-dev-software() {
     echo -e "${YELLOW}Install NPM${CLEAR}"
     sudo -E apt -y install npm
 
+    ## NVM
+    echo -e "${YELLOW}Install NVM${CLEAR}"
+    export NVM_DIR="$HOME/.nvm" && (
+        git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+        cd "$NVM_DIR"
+        git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1))
+    ) && \. "$NVM_DIR/nvm.sh"
+    echo 'export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >>~./bashrc
+    source ~/.bashrc
+    command -v nvm
+
     ## Node
     echo -e "${YELLOW}Install Node${CLEAR}"
-    sudo curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-    sudo apt install -y nodejs
+    nvm install node
 
     ## Pip3
     echo -e "${YELLOW}Set Pip3${CLEAR}"
